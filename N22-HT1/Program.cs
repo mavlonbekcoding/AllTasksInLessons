f46867808d97d0a4fbcab3c6cd2d76b6dfa336d6
Mavlonbek-PC <Mavlonbek-PC@MAVLONBEK>
2023-08-23 03:00:05 +0500

    Add project files.

---
+++ N22-HT1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrimeNumberSorting
+{
+    class Program
+    {
+        static bool IsPrime(int number)
+        {
+            if (number <= 1)
+                return false;
+
+            for (int i = 2; i * i <= number; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 7, 2, 15, 3, 11, 8, 5 };
+
+            var primeNumbers = numbers.Where(IsPrime).OrderByDescending(num => num).ToList();
+
+            Console.WriteLine("Tub sonlar kamayish tartibida: ");
+            foreach (var prime in primeNumbers)
+            {
+                Console.Write(prime + " ");
+            }
+        }
+    }
+}
