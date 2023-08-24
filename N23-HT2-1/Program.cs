764e60f4195fad66602bea9478953de38a79ffd6
Mavlonbek Isroilov <isroilovmavlonbekk@outlook.com>
2023-08-25 04:55:38 +0500

    ok

---
+++ N23-HT2-1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace N23_HT2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Users = new List<User>
+            {
+                new User("John", "Doe", "johndoe@example.com"),
+                new User("Jane", "Doe", "janedoe@example.com"),
+                new User("Bob", "Smith", "bobsmith@example.com"),
+                new User("Alice", "Johnson", "alicejohnson@example.com"),
+                new User("Mike", "Brown", "mikebrown@example.com"),
+                new User("Emily", "Davis", "emilydavis@example.com"),
+                new User("David", "Wilson", "davidwilson@example.com"),
+                new User("Sarah", "Taylor", "sarahtaylor@example.com"),
+                new User("Tom", "Anderson", "tomanderson@example.com"),
+                new User("Lisa", "Thomas", "lisathomas@example.com")
+            };
+
+            Console.Write("So'zni kiriting: ");
+            string key = Console.ReadLine().ToLower();
+
+            var matchingUsers = Users
+                .Where(user => user.FirstName.ToLower().Contains(key) || user.LastName.ToLower().Contains(key))
+                .Take(5);
+
+            if (matchingUsers.Any())
+            {
+                Console.WriteLine("\nMa'lumot topildi:");
+                foreach (User user in matchingUsers)
+                {
+                    Console.WriteLine($"{user.FirstName} {user.LastName}, {user.EmailAddress}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nMa'lumot topilmadi.");
+            }
+        }
+    }
+}
---
+++ N23-HT2-1/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N23_HT2_1
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+
+        public User(string FirstName, string LastName, string EmailAddress) 
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.EmailAddress = EmailAddress;
+        }
+    }
+
+}
