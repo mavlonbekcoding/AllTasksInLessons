3e14f5da084e2bbb6711b494f3f301a1f16f6499
Mavlonbek Isroilov <isroilovmavlonbekk@outlook.com>
2023-10-13 10:24:12 +0500

    ok men yomon

---
+++ CT-1/Program.cs
@@ -0,0 +1,18 @@
+﻿string baseFolder = @"D:\\Test\";
+
+for (int i = 1; i <= 3; i++)
+{
+    string subFolderName = $"test{i}";
+
+    // Asosiy papkani yaratamiz
+    DirectoryInfo mainFolder = new DirectoryInfo(Path.Combine(baseFolder, subFolderName));
+    mainFolder.Create();
+
+    // Har bir asosiy papka ichiga 5 marta folder yaratamiz
+    for (int j = 1; j <= 3; j++)
+    {
+        string subFolderName = $"test{j}";
+        DirectoryInfo subFolder = new DirectoryInfo(Path.Combine(mainFolder.FullName, subFolderName));
+        subFolder.Create();
+    }
+}
